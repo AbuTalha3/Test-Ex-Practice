59fd7caff842a9ab09c3fee8a9b68b195786c862
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-07-20 11:36:52 +0500

    Write capitalize string and test it

---
+++ capitalize.test.js
@@ -0,0 +1,11 @@
+const capitalize = require('./capitalize');
+
+describe('capitalize', () => {
+  it('should capitalize the first character of a string', () => {
+    const string = 'abutalha';
+    const output = 'Abutalha';
+    const result = capitalize(string);
+
+    expect(result).toBe(utput);
+  });
+});
