c5d3463efc6ddcabc38c7fd568494a7780c236db
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-07-20 11:14:46 +0500

    Write calculator functions .

---
+++ calc.js
@@ -0,0 +1,18 @@
+class calc {
+  add(a, b) {
+    return a + b;
+  }
+  subtract(a, b) {
+    return a - b;
+  }
+  multiply(a, b) {
+    return a * b;
+  }
+  divide(a, b) {
+    if (b === 0) {
+      throw new Error('Cannot divide by zero');
+    }
+    return a / b;
+  }
+  
+}
